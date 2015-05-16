3dcfb84b9642d683080bd18124a668a7a1ab385f
Brad Montgomery <brad@bradmontgomery.net>
2015-05-15 19:01:40 -0500

    remove copy/paste cruft

--- src/main/java/org/tndata/android/grow/util/ViewHelper.java
+++ src/main/java/org/tndata/android/grow/util/ViewHelper.java
@@ -3,8 +3,6 @@ package org.tndata.android.grow.util;
 import android.view.View;
 
 public class ViewHelper {
-    public final static int SELECTED = 1;
-    public final static int ADD = 2;
 
     /**
      * Helper method to set the visibility for a View, but only if the view is
