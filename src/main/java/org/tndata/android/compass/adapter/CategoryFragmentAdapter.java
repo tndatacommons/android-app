93b8f181ca98cab709cf334a1f4b98cdc9277487
Brad Montgomery <brad@bradmontgomery.net>
2015-06-08 14:11:39 -0500

    temporarity disabling the menu to remove goals

--- src/main/java/org/tndata/android/compass/adapter/CategoryFragmentAdapter.java
+++ src/main/java/org/tndata/android/compass/adapter/CategoryFragmentAdapter.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -67,9 +66,6 @@ public class CategoryFragmentAdapter extends
         }
 
         public void toggleCard() {
-
-            Log.d("CategoryFragmentAdapter", "Toggling Card");
-
             // If the card is collapsed, expand it; if it's expanded, collapse it.
             if(descriptionTextView.getVisibility() == View.GONE) {
                 circleView.setVisibility(View.GONE);
