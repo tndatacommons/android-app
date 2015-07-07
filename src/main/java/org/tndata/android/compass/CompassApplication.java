3baa80263e1baade30523d32065e664f78987ce7
Brad Montgomery <brad@bradmontgomery.net>
2015-07-07 14:05:55 -0500

    comments and logging

--- src/main/java/org/tndata/android/compass/CompassApplication.java
+++ src/main/java/org/tndata/android/compass/CompassApplication.java
@@ -1,6 +1,7 @@
 package org.tndata.android.compass;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.tndata.android.compass.model.Action;
 import org.tndata.android.compass.model.Behavior;
@@ -11,12 +12,18 @@ import org.tndata.android.compass.model.User;
 import java.util.ArrayList;
 
 public class CompassApplication extends Application {
+    private String TAG = "CompassApplication";
     private String mToken;
-    private User mUser;
-    private ArrayList<Category> mCategories = new ArrayList<Category>();
-    private ArrayList<Goal> mGoals = new ArrayList<Goal>();
+    private User mUser; // The logged-in user
+    private ArrayList<Category> mCategories = new ArrayList<Category>(); // The user's selected Categories
+    private ArrayList<Goal> mGoals = new ArrayList<Goal>();  // The user's selected Goals
     private ArrayList<Behavior> mBehaviors = new ArrayList<Behavior>(); // The user's selected behaviors
-    private ArrayList<Action> mActions = new ArrayList<Action>();
+    private ArrayList<Action> mActions = new ArrayList<Action>(); // The user's selected actions
+
+    // Need
+    // - Goals organized by Category
+    // - Behaviors organized by Goal
+    // - Actions organized by Behavior
 
     public CompassApplication() {
         super();
@@ -44,8 +51,12 @@ public class CompassApplication extends Application {
 
     public void setCategories(ArrayList<Category> categories) {
         mCategories = categories;
+        for(Category c: categories) {
+            Log.d(TAG, "--> setCategory: " + c.getTitle());
+        }
     }
 
+    /* Remove a single Category from the user's collection */
     public void removeCategory(Category category) {
         mCategories.remove(category);
     }
@@ -56,20 +67,28 @@ public class CompassApplication extends Application {
 
     public void setGoals(ArrayList<Goal> goals) {
         mGoals = goals;
+        for(Goal g: goals) {
+            Log.d(TAG, "--> setGoals: " + g.getTitle());
+        }
     }
 
+    /* Remove a single Goal from the user's collection */
     public void removeGoal(Goal goal) {
         mGoals.remove(goal);
     }
 
     public void setBehaviors(ArrayList<Behavior> behaviors) {
         mBehaviors = behaviors;
+        for(Behavior b: behaviors) {
+            Log.d(TAG, "--> setBehavior: " + b.getTitle());
+        }
     }
 
     public ArrayList<Behavior> getBehaviors() {
         return mBehaviors;
     }
 
+    /* Remove a single Behavior from a user's collection */
     public void removeBehavior(Behavior behavior) {
         mBehaviors.remove(behavior);
     }
@@ -82,14 +101,19 @@ public class CompassApplication extends Application {
         return mActions;
     }
 
+    /* Remove a single Action from a user's collection */
     public void removeAction(Action action) {
         mActions.remove(action);
     }
 
     public void setActions(ArrayList<Action> actions) {
         mActions = actions;
+        for(Action a: actions) {
+            Log.d(TAG, "--> setAction: " + a.getTitle());
+        }
     }
 
+    /* update a single Action in a user's collection */
     public void updateAction(Action action) {
         // Given a single action, find it in the list of Actions and keep the input version
         int i = 0;
