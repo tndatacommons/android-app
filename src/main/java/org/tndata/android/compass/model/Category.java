440721a33796e4f4faa9ed7114bc358bebdf18e3
Brad Montgomery <brad@bradmontgomery.net>
2015-07-09 09:15:06 -0500

    a Category should be able to add individual Goal instances to its collection

--- src/main/java/org/tndata/android/compass/model/Category.java
+++ src/main/java/org/tndata/android/compass/model/Category.java
@@ -84,6 +84,12 @@ public class Category extends TDCBase implements Serializable,
         this.goals = goals;
     }
 
+    public void addGoal(Goal goal) {
+        if(!this.goals.contains(goal)) {
+            this.goals.add(goal);
+        }
+    }
+
     public void removeGoal(Goal goal) {
         if(this.goals.contains(goal)) {
             this.goals.remove(goal);
