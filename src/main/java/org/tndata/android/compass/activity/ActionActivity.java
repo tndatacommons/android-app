5e73813c1776b9bd2d73ffa92ea2f513a4978a41
Ismael Alonso <ismaha91@gmail.com>
2015-07-23 15:46:49 -0500

    Populate some sections of the activity

--- src/main/java/org/tndata/android/compass/activity/ActionActivity.java
+++ src/main/java/org/tndata/android/compass/activity/ActionActivity.java
@@ -2,10 +2,13 @@ package org.tndata.android.compass.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.tndata.android.compass.R;
 import org.tndata.android.compass.model.Action;
+import org.tndata.android.compass.util.ImageLoader;
 
 
 /**
@@ -15,18 +18,60 @@ import org.tndata.android.compass.model.Action;
  * @author Ismael Alonso
  * @version 1.0.0
  */
-public class ActionActivity extends ActionBarActivity{
+public class ActionActivity extends ActionBarActivity implements View.OnClickListener{
     //The action in question
     private Action mAction;
-
-    //UI components
-    private ImageView mActionImage;
-    private TextView mActionTitle;
-    private TextView mActionDescription;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        //TODO fetch the action (download?)
+        mAction = new Action();
+
+        //Fetch UI components
+        ImageView actionImage = (ImageView)findViewById(R.id.action_image);
+        TextView actionTitle = (TextView)findViewById(R.id.action_title);
+        TextView actionDescription = (TextView)findViewById(R.id.action_description);
+
+        //Populate UI
+        //ImageLoader.loadBitmap(actionImage, mAction.getIconUrl(), false);
+        actionTitle.setText(mAction.getTitle());
+        actionDescription.setText(mAction.getDescription());
+
+        //Listeners
+        findViewById(R.id.action_later).setOnClickListener(this);
+        findViewById(R.id.cancel_button).setOnClickListener(this);
+        findViewById(R.id.action_did_it).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.action_later:
+                later();
+                break;
+
+            case R.id.action_cancel:
+                cancel();
+                break;
+
+            case R.id.action_did_it:
+                didIt();
+                break;
+        }
+    }
+
+    private void later(){
+
+    }
+
+    private void cancel(){
+
+    }
+
+    private void didIt(){
+
     }
 }
