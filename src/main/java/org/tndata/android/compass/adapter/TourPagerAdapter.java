d1c9849f5ae830040ce312430e338305bb038c02
Damian Urbanczyk <damianurbanczyk@gmail.com>
2015-08-07 14:58:36 +0200

    App Tour Revisions

--- src/main/java/org/tndata/android/compass/adapter/TourPagerAdapter.java
+++ src/main/java/org/tndata/android/compass/adapter/TourPagerAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.tndata.android.compass.R;
 
@@ -14,7 +15,8 @@ public class TourPagerAdapter extends PagerAdapter {
 
     Context mContext;
     LayoutInflater mLayoutInflater;
-    int[] images = new int[]{R.drawable.onboarding1, R.drawable.onboarding2, R.drawable.onboarding3, R.drawable.onboarding4};
+    int[] images = new int[]{R.drawable.tour1, R.drawable.tour2, R.drawable.tour3, R.drawable.tour4};
+    int[] informations = new int[]{R.string.tour_page_1_information, R.string.tour_page_2_information, R.string.tour_page_3_information, R.string.tour_page_4_information};
 
 
     public TourPagerAdapter(Context context) {
@@ -36,6 +38,17 @@ public class TourPagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         View itemView = mLayoutInflater.inflate(R.layout.tour_image_item, container, false);
 
+        TextView textViewBottom = (TextView) itemView.findViewById(R.id.tour_information_bottom);
+        TextView textViewTop = (TextView) itemView.findViewById(R.id.tour_information_top);
+        if (position == 2) {
+            textViewBottom.setVisibility(View.VISIBLE);
+            textViewBottom.setText(mContext.getString(informations[position]));
+            textViewTop.setVisibility(View.GONE);
+        } else {
+            textViewTop.setVisibility(View.VISIBLE);
+            textViewTop.setText(mContext.getString(informations[position]));
+            textViewBottom.setVisibility(View.GONE);
+        }
         ImageView imageView = (ImageView) itemView.findViewById(R.id.tourImageView);
         imageView.setImageResource(images[position]);
         container.addView(itemView);
