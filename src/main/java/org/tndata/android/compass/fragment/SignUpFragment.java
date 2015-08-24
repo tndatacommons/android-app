3f8d7337957c2cd414902d9e792be9236ce01786
Ismael Alonso <ismaha91@gmail.com>
2015-08-24 14:27:35 -0500

    Add progress report to and rewrote sign up

--- src/main/java/org/tndata/android/compass/fragment/SignUpFragment.java
+++ src/main/java/org/tndata/android/compass/fragment/SignUpFragment.java
@@ -20,228 +20,248 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class SignUpFragment extends Fragment implements SignUpTaskListener {
-    private Button mSignUpButton;
-    private TextView mSignUpEmailButton;
-    private TextView mErrorTextView;
-    private EditText mEmailEditText;
-    private EditText mPasswordEditText;
-    private EditText mPasswordConfirmEditText;
-    private EditText mFirstNameEditText;
-    private EditText mLastNameEditText;
-    private RelativeLayout mEmailContainerView;
-    private ImageView mPrivacyButtonImageView;
-    private String mErrorString = "";
-    private boolean mEmailViewIsHidden = false;
-    private SignUpFragmentListener mCallback;
 
-    public interface SignUpFragmentListener {
-        public void signUpSuccess(User user);
+public class SignUpFragment extends Fragment implements SignUpTaskListener, OnClickListener{
+    //Listener interface.
+    private SignUpFragmentListener mListener;
 
-        public void showTermsAndConditions();
-    }
+    //UI components
+    private LinearLayout mSignupContainer;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mConfirmPassword;
+    private EditText mFirstName;
+    private EditText mLastName;
+    private TextView mError;
+    private ProgressBar mProgress;
+    private Button mSignUp;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View v = getActivity().getLayoutInflater().inflate(
-                R.layout.fragment_signup, container, false);
-        mPrivacyButtonImageView = (ImageView) v
-                .findViewById(R.id.signup_privacy_button_imageview);
-        mPrivacyButtonImageView.setOnClickListener(new OnClickListener() {
+    private String mErrorString;
+    private boolean mEmailViewIsHidden;
 
-            @Override
-            public void onClick(View v) {
-                mCallback.showTermsAndConditions();
-            }
-        });
-        mEmailEditText = (EditText) v.findViewById(R.id.signup_email_edittext);
-        mPasswordEditText = (EditText) v
-                .findViewById(R.id.signup_password_edittext);
-        mPasswordConfirmEditText = (EditText) v
-                .findViewById(R.id.signup_password_confirm_edittext);
-        mFirstNameEditText = (EditText) v
-                .findViewById(R.id.signup_first_name_edittext);
-        mLastNameEditText = (EditText) v
-                .findViewById(R.id.signup_last_name_edittext);
-        mErrorTextView = (TextView) v.findViewById(R.id.signup_error_textview);
-        mSignUpButton = (Button) v.findViewById(R.id.signup_signup_button);
-        mSignUpButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                doSignUp();
-            }
-        });
-        mSignUpEmailButton = (TextView) v
-                .findViewById(R.id.signup_email_button);
-        mSignUpEmailButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                showEmailView();
-            }
-        });
-        mEmailContainerView = (RelativeLayout) v
-                .findViewById(R.id.signup_email_container);
-        hideEmailView();
-        return v;
-    }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity); // This makes sure that the container activity
-        // has implemented the callback interface. If not, it throws an
-        // exception
-        try {
-            mCallback = (SignUpFragmentListener) activity;
-        } catch (ClassCastException e) {
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+        //This makes sure that the host activity has implemented the callback interface.
+        //  If not, it throws an exception
+        try{
+            mListener = (SignUpFragmentListener)activity;
+        }
+        catch (ClassCastException ccx){
             throw new ClassCastException(activity.toString()
                     + " must implement SignUpFragmentListener");
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mCallback = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        //First screen UI components
+        ImageView info = (ImageView)rootView.findViewById(R.id.signup_info);
+        TextView signUpWithEmail = (TextView)rootView.findViewById(R.id.signup_with_email);
+
+        info.setOnClickListener(this);
+        signUpWithEmail.setOnClickListener(this);
+
+        //Second screen UI components
+        mSignupContainer = (LinearLayout)rootView.findViewById(R.id.signup_container);
+        mEmail = (EditText)rootView.findViewById(R.id.signup_email);
+        mPassword = (EditText)rootView.findViewById(R.id.signup_password);
+        mConfirmPassword = (EditText)rootView.findViewById(R.id.signup_confirm_password);
+        mFirstName = (EditText)rootView.findViewById(R.id.signup_first_name);
+        mLastName = (EditText)rootView.findViewById(R.id.signup_last_name);
+        mError = (TextView)rootView.findViewById(R.id.signup_error);
+        mProgress = (ProgressBar)rootView.findViewById(R.id.signup_progress);
+        mSignUp = (Button)rootView.findViewById(R.id.signup_button);
+
+        mSignUp.setOnClickListener(this);
+
+        mEmailViewIsHidden = false;
+        hideEmailView();
+
+        mErrorString = "";
+
+        return rootView;
     }
 
     @Override
-    public void signUpResult(User result) {
-        if (result != null) {
-            mCallback.signUpSuccess(result);
-        } else {
-            mErrorString = getActivity().getResources().getString(
-                    R.string.signup_auth_error);
-            mErrorTextView.setText(mErrorString);
-            mErrorTextView.setVisibility(View.VISIBLE);
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.signup_info:
+                mListener.showTermsAndConditions();
+                break;
+
+            case R.id.signup_with_email:
+                showEmailView();
+                break;
+
+            case R.id.signup_button:
+                doSignUp();
         }
     }
 
-    private void doSignUp() {
-        String emailAddress = mEmailEditText.getText().toString();
-        String password = mPasswordEditText.getText().toString();
-        String confirmPassword = mPasswordConfirmEditText.getText().toString();
-        String firstName = mFirstNameEditText.getText().toString();
-        String lastName = mLastNameEditText.getText().toString();
-        if (isValidEmail(emailAddress)
-                && confirmPasswords(password, confirmPassword)) {
-            mErrorTextView.setVisibility(View.INVISIBLE);
+    private void showEmailView(){
+        if (mEmailViewIsHidden){
+            int height = mSignupContainer.getMeasuredHeight();
+            ObjectAnimator anim = ObjectAnimator.ofFloat(mSignupContainer,
+                    "translationY", height, 0);
+            anim.setDuration(500);
+
+            anim.addListener(new AnimatorListener(){
+                @Override
+                public void onAnimationStart(Animator animation){
+                    mSignupContainer.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation){
+                    mEmailViewIsHidden = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation){
+                    //Unused
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation){
+                    //Unused
+                }
+            });
+
+            mSignupContainer.setVisibility(View.VISIBLE);
+            anim.start();
+        }
+    }
+
+    public void hideEmailView(){
+        if (!mEmailViewIsHidden){
+            int height = mSignupContainer.getMeasuredHeight();
+            ObjectAnimator anim = ObjectAnimator.ofFloat(mSignupContainer,
+                    "translationY", 0, height);
+            anim.setDuration(500);
+
+            anim.addListener(new AnimatorListener(){
+                @Override
+                public void onAnimationStart(Animator animation){
+                    //Unused
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation){
+                    mSignupContainer.setVisibility(View.GONE);
+                    mEmailViewIsHidden = true;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation){
+                    //Unused
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation){
+                    //Unused
+                }
+            });
+
+            anim.start();
+        }
+    }
+
+    private void doSignUp(){
+        String emailAddress = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        String confirmPassword = mConfirmPassword.getText().toString();
+        String firstName = mFirstName.getText().toString();
+        String lastName = mLastName.getText().toString();
+        if (isValidEmail(emailAddress) && confirmPasswords(password, confirmPassword)){
+            mError.setVisibility(View.INVISIBLE);
+            mProgress.setVisibility(View.VISIBLE);
+            mSignUp.setEnabled(false);
             mErrorString = "";
-            mErrorTextView.setText(mErrorString);
+            mError.setText(mErrorString);
             User user = new User();
             user.setEmail(emailAddress);
             user.setPassword(password);
             user.setFirstName(firstName);
             user.setLastName(lastName);
             user.onBoardingComplete(false);
-            new SignUpTask(this).executeOnExecutor(
-                    AsyncTask.THREAD_POOL_EXECUTOR, user);
-        } else {
-            mErrorTextView.setText(mErrorString);
-            mErrorTextView.setVisibility(View.VISIBLE);
+            new SignUpTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, user);
+        }
+        else{
+            mError.setText(mErrorString);
+            mError.setVisibility(View.VISIBLE);
+            mProgress.setVisibility(View.GONE);
         }
     }
 
-    private boolean confirmPasswords(String passwordA, String passwordB) {
-        if (passwordA.equals(passwordB)) {
-            if (passwordA.length() >= 5) {
+    private boolean confirmPasswords(String passwordA, String passwordB){
+        if (passwordA.equals(passwordB)){
+            if (passwordA.length() >= 5){
                 mErrorString = "";
                 return true;
-            } else {
+            }
+            else{
                 mErrorString = getActivity().getResources().getString(
                         R.string.signup_password_length_error);
                 return false;
             }
-        } else {
+        }
+        else{
             mErrorString = getActivity().getResources().getString(
                     R.string.signup_password_mismatch_error);
             return false;
         }
     }
 
-    private boolean isValidEmail(CharSequence target) {
-        if (target == null) {
+    private boolean isValidEmail(CharSequence target){
+        if (target == null){
             return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(target).matches()) {
-            mErrorString = getActivity().getResources().getString(
-                    R.string.login_email_error);
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(target).matches()){
+            mErrorString = getActivity().getResources().getString(R.string.login_email_error);
             return false;
-        } else {
+        }
+        else{
             mErrorString = "";
             return true;
         }
     }
 
-    public boolean isEmailViewShown() {
+    @Override
+    public void signUpResult(User result){
+        if (result != null){
+            mListener.signUpSuccess(result);
+        }
+        else{
+            mErrorString = getActivity().getResources().getString(R.string.signup_auth_error);
+            mError.setText(mErrorString);
+            mError.setVisibility(View.VISIBLE);
+            mProgress.setVisibility(View.GONE);
+            mSignUp.setEnabled(true);
+        }
+    }
+
+    public boolean isEmailViewShown(){
         return !mEmailViewIsHidden;
     }
 
-    private void showEmailView() {
-        if (mEmailViewIsHidden) {
-            int height = mEmailContainerView.getMeasuredHeight();
-            ObjectAnimator anim = ObjectAnimator.ofFloat(mEmailContainerView,
-                    "translationY", height, 0);
-            anim.setDuration(500);
-
-            anim.addListener(new AnimatorListener() {
-
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                }
-
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mEmailViewIsHidden = false;
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-                }
-
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    mEmailContainerView.setVisibility(View.VISIBLE);
-                }
-            });
-
-            mEmailContainerView.setVisibility(View.VISIBLE);
-            anim.start();
-        }
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        mListener = null;
     }
 
-    public void hideEmailView() {
-        if (!mEmailViewIsHidden) {
-            int height = mEmailContainerView.getMeasuredHeight();
-            ObjectAnimator anim = ObjectAnimator.ofFloat(mEmailContainerView,
-                    "translationY", 0, height);
-            anim.setDuration(500);
-            anim.start();
-            anim.addListener(new AnimatorListener() {
 
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                }
+    public interface SignUpFragmentListener{
+        public void signUpSuccess(User user);
 
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mEmailContainerView.setVisibility(View.GONE);
-                    mEmailViewIsHidden = true;
-                }
-
-                @Override
-                public void onAnimationRepeat(Animator animation) {
-                }
-
-                @Override
-                public void onAnimationStart(Animator animation) {
-                }
-            });
-        }
+        public void showTermsAndConditions();
     }
-
 }
