75f67f91f93d3545b93e088d700a79929393b19e
Ismael Alonso <ismaha91@gmail.com>
2015-08-24 09:45:06 -0500

    Create sources infrastructure in settings

--- src/main/java/org/tndata/android/compass/fragment/SettingsFragment.java
+++ src/main/java/org/tndata/android/compass/fragment/SettingsFragment.java
@@ -9,51 +9,81 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
-    private OnSettingsClickListener mCallback;
 
-    public interface OnSettingsClickListener {
-        public void logOut();
-    }
+/**
+ * Fragment that contains the UI for settings. Triggers settings events.
+ */
+public class SettingsFragment extends PreferenceFragment implements OnPreferenceClickListener{
+    //The listener interface
+    private OnSettingsClickListener mListener;
+
+    //A list of clickable preferences
+    private Preference mLogOut;
+    private Preference mSources;
+
 
     @Override
-    public void onAttach(Activity activity) {
+    public void onAttach(Activity activity){
         super.onAttach(activity);
 
-        // This makes sure that the container activity has implemented
-        // the callback interface. If not, it throws an exception
-        try {
-            mCallback = (OnSettingsClickListener) activity;
-        } catch (ClassCastException e) {
+        //This makes sure that the container activity has implemented the callback
+        //  interface. If not, it throws an exception.
+        try{
+            mListener = (OnSettingsClickListener)activity;
+        }
+        catch (ClassCastException ccx){
             throw new ClassCastException(activity.toString()
                     + " must implement OnSettingsClickListener");
         }
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-        Preference logOut = findPreference("pref_key_logout");
-        logOut.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                mCallback.logOut();
-                return false;
-            }
-        });
-
+        mLogOut = findPreference("pref_key_logout");
+        mLogOut.setOnPreferenceClickListener(this);
         String displayName = "";
-        try {
-            displayName = ((CompassApplication) getActivity().getApplication())
-                    .getUser().getFullName();
-        } catch (Exception e) {
-            logOut.setSummary("");
+        try{
+            displayName = ((CompassApplication)getActivity().getApplication()).getUser().getFullName();
         }
-        logOut.setSummary(getActivity().getResources().getString(
+        catch (Exception x){
+            mLogOut.setSummary("");
+        }
+        mLogOut.setSummary(getActivity().getResources().getString(
                 R.string.settings_logout_summary, displayName));
 
+        mSources = findPreference("pref_key_sources");
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference){
+        if (preference == mLogOut){
+            mListener.logOut();
+        }
+        else if (preference == mSources){
+            mListener.sources();
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Listener interface to handle preference click events int the host activity.
+     */
+    public interface OnSettingsClickListener{
+        /**
+         * Called when the log out preference is clicked.
+         */
+        void logOut();
+
+        /**
+         * Called when the sources preference is clicked.
+         */
+        void sources();
     }
 }
