c60ffdd7745f13ba0534d3e7d380f08d23f3b020
Ismael Alonso <ismaha91@gmail.com>
2015-08-28 09:38:36 -0500

    GoalFilter improvement

--- src/main/java/org/tndata/android/compass/util/GoalFilter.java
+++ src/main/java/org/tndata/android/compass/util/GoalFilter.java
@@ -4,10 +4,10 @@ import android.widget.Filter;
 
 import org.tndata.android.compass.adapter.ChooseGoalsAdapter;
 import org.tndata.android.compass.model.Goal;
-import org.tndata.android.compass.model.TDCBase;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Filters goals by title.
@@ -15,15 +15,16 @@ import java.util.List;
  * @author Ismael Alonso
  * @version 1.0.0
  */
-public class GoalFilter<T extends TDCBase> extends Filter{
+public class GoalFilter extends Filter{
     private ChooseGoalsAdapter mAdapter;
     private List<Goal> mList;
 
+    
     /**
      * Constructor.
      *
-     * @param adapter the adapter
-     * @param list
+     * @param adapter the adapter to be filtered.
+     * @param list the original list of the adapter.
      */
     public GoalFilter(ChooseGoalsAdapter adapter, List<Goal> list){
         mAdapter = adapter;
@@ -32,8 +33,6 @@ public class GoalFilter<T extends TDCBase> extends Filter{
 
     @Override
     protected FilterResults performFiltering(CharSequence constraint){
-        FilterResults results = new FilterResults();
-
         List<Goal> output = new ArrayList<>();
         for (Goal goal:mList){
             if (goal.getTitle().contains(constraint)){
@@ -41,16 +40,33 @@ public class GoalFilter<T extends TDCBase> extends Filter{
             }
         }
 
-        results.count = output.size();
-        results.values = output;
-
-        return results;
+        return new GoalFilterResults(output);
     }
 
     @Override
     protected void publishResults(CharSequence constraint, FilterResults results){
-        if (results.values instanceof List){
-            mAdapter.addGoals((List<Goal>)results.values);
+        if (results instanceof GoalFilterResults){
+            mAdapter.addGoals(((GoalFilterResults)results).mOutput);
+        }
+    }
+
+
+    /**
+     * An extension of the FilterResults class that contains a list of goals.
+     *
+     * @author Ismael Alonso
+     * @version 1.0.0
+     */
+    private class GoalFilterResults extends FilterResults{
+        private List<Goal> mOutput;
+
+        /**
+         * Constructor.
+         *
+         * @param output the result of the filtering.
+         */
+        GoalFilterResults(List<Goal> output){
+            mOutput = output;
         }
     }
 }
