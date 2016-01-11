7027e158a0c354b58156b39128a218111bdbe69b
Ismael Alonso <ismaha91@gmail.com>
2016-01-11 15:00:13 -0600

    Straighten up BASE_URL

--- src/main/java/org/tndata/android/compass/util/API.java
+++ src/main/java/org/tndata/android/compass/util/API.java
@@ -30,7 +30,7 @@ public abstract class API{
             USE_NGROK_TUNNEL ?
                     NGROK_TUNNEL_URL
                     :
-                    !BuildConfig.DEBUG ?
+                    BuildConfig.DEBUG ?
                             TNDATA_STAGING_URL
                             :
                             TNDATA_BASE_URL;
