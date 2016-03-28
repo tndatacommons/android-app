fbdf1d88a108100216664b0b683a240212260b9c
Ismael Alonso <ismaha91@gmail.com>
2016-03-28 12:48:09 -0500

    Remove unnecessary imports from FeedUtil

--- src/main/java/org/tndata/android/compass/adapter/feed/FeedUtil.java
+++ src/main/java/org/tndata/android/compass/adapter/feed/FeedUtil.java
@@ -1,14 +1,9 @@
 package org.tndata.android.compass.adapter.feed;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.view.View;
 
 import org.tndata.android.compass.R;
-import org.tndata.android.compass.model.UpcomingAction;
-import org.tndata.android.compass.service.ActionReportService;
 import org.tndata.android.compass.ui.CompassPopupMenu;
 
 
