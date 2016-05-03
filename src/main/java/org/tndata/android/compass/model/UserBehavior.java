d4bb885edf1d8418bd045927f470b32923dd87c3
Ismael Alonso <ismaha91@gmail.com>
2016-05-03 15:23:00 -0500

    UserBehavior -> Parcelable

--- src/main/java/org/tndata/android/compass/model/UserBehavior.java
+++ src/main/java/org/tndata/android/compass/model/UserBehavior.java
@@ -1,10 +1,12 @@
 package org.tndata.android.compass.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 import org.tndata.android.compass.parser.ParserModels;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +17,7 @@ import java.util.List;
  * @author Ismael Alonso
  * @version 1.0.0
  */
-public class UserBehavior extends UserContent implements Serializable, ParserModels.ResultSet{
-    private static final long serialVersionUID = 774798265423822842L;
-
+public class UserBehavior extends UserContent implements Parcelable, ParserModels.ResultSet{
     public static final String TYPE = "userbehavior";
 
 
@@ -144,5 +144,33 @@ public class UserBehavior extends UserContent implements Serializable, ParserMod
     @Override
     public String toString(){
         return "UserBehavior #" + getId() + " (" + mBehavior.toString() + ")";
+    }
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+        super.addToParcel(dest, flags);
+        dest.writeParcelable(mBehavior, flags);
+    }
+
+    public static final Creator<UserBehavior> CREATOR = new Creator<UserBehavior>(){
+        @Override
+        public UserBehavior createFromParcel(Parcel source){
+            return new UserBehavior(source);
+        }
+
+        @Override
+        public UserBehavior[] newArray(int size){
+            return new UserBehavior[size];
+        }
+    };
+
+    private UserBehavior(Parcel src){
+        super(src);
+        mBehavior = src.readParcelable(TDCBehavior.class.getClassLoader());
     }
 }
