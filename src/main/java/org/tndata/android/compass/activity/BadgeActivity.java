d96413ec3e6f1b36f8f1c782a7231b3577b1c1ff
Ismael Alonso <ismaha91@gmail.com>
2016-07-01 15:14:11 -0500

    Add share button to BadgeActivity

--- src/main/java/org/tndata/android/compass/activity/BadgeActivity.java
+++ src/main/java/org/tndata/android/compass/activity/BadgeActivity.java
@@ -1,5 +1,6 @@
 package org.tndata.android.compass.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +22,18 @@ import org.tndata.android.compass.util.ImageLoader;
  * @author Ismael Alonso
  * @version 1.0.0
  */
-public class BadgeActivity extends AppCompatActivity implements ImageLoader.ImageLoaderCallback{
+public class BadgeActivity
+        extends AppCompatActivity
+        implements
+                View.OnClickListener,
+                ImageLoader.ImageLoaderCallback{
+
     private static final String TAG = "BadgeActivity";
 
     public static final String BADGE_KEY = "org.tndata.compass.BadgeActivity.Badge";
 
+
+    private Badge mBadge;
 
     private View mImageFrame;
     private ImageView mImage;
@@ -36,21 +44,37 @@ public class BadgeActivity extends AppCompatActivity implements ImageLoader.Imag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_badge);
 
-        Badge badge = getIntent().getParcelableExtra(BADGE_KEY);
+        mBadge = getIntent().getParcelableExtra(BADGE_KEY);
 
         mImageFrame = findViewById(R.id.badge_image_frame);
         mImage = (ImageView)findViewById(R.id.badge_image);
         TextView name = (TextView)findViewById(R.id.badge_name);
         TextView description = (TextView)findViewById(R.id.badge_description);
+        View share = findViewById(R.id.badge_share);
 
+        //Warning killers...
         assert name != null;
         assert description != null;
+        assert share != null;
 
         ImageLoader.Options options = new ImageLoader.Options().setUseDefaultPlaceholder(false);
-        ImageLoader.loadBitmap(mImage, badge.getImageUrl(), options, this);
+        ImageLoader.loadBitmap(mImage, mBadge.getImageUrl(), options, this);
 
-        name.setText(getString(R.string.badge_header, badge.getName().toUpperCase()));
-        description.setText(badge.getDescription());
+        name.setText(getString(R.string.badge_header, mBadge.getName().toUpperCase()));
+        description.setText(mBadge.getDescription());
+        share.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.badge_share){
+            String content = getString(R.string.badge_share, mBadge.getName());
+
+            Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, content);
+            startActivity(Intent.createChooser(shareIntent, "Share via"));
+        }
     }
 
     @Override
