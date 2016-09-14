9a02dfafcd84d8f8b27484411673950d37fcd2d6
Ismael Alonso <ismaha91@gmail.com>
2016-09-14 17:37:35 -0500

    Make items in the editable list editable

--- src/main/java/org/tndata/android/compass/holder/EditableListCardHolder.java
+++ src/main/java/org/tndata/android/compass/holder/EditableListCardHolder.java
@@ -1,14 +1,23 @@
 package org.tndata.android.compass.holder;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.tndata.android.compass.R;
 import org.tndata.android.compass.databinding.CardEditableListBinding;
@@ -108,17 +117,124 @@ public class EditableListCardHolder extends RecyclerView.ViewHolder implements V
     }
 
 
-    private class ItemHolder extends RecyclerView.ViewHolder{
+    private class ItemHolder
+            extends RecyclerView.ViewHolder
+            implements
+                    TextView.OnEditorActionListener,
+                    View.OnClickListener,
+                    DialogInterface.OnClickListener{
+
         private ItemEditableListEntryBinding mBinding;
+
+        private Drawable mTitleDefaultBackground;
 
 
         public ItemHolder(ItemEditableListEntryBinding binding){
             super(binding.getRoot());
             mBinding = binding;
+            mBinding.editableListEntryEdit.setOnClickListener(this);
+            mBinding.editableListEntrySave.setOnClickListener(this);
+
+            mTitleDefaultBackground = mBinding.editableListEntryTitle.getBackground();
+
+            mBinding.editableListEntryTitle.setFocusable(false);
+            mBinding.editableListEntryTitle.setBackgroundResource(0);
         }
 
         public void setTitle(String title){
             mBinding.editableListEntryTitle.setText(title);
+        }
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+            if (actionId == EditorInfo.IME_ACTION_DONE){
+                if (mBinding.editableListEntryTitle.getText().toString().trim().length() > 0){
+                    onClick(mBinding.editableListEntrySave);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public void onClick(DialogInterface dialog, int which){
+            //Edit the title
+            if (which == 0){
+                EditText title = mBinding.editableListEntryTitle;
+                //Set the title click listener to null to avoid going into the trigger editor
+                title.setOnClickListener(null);
+                //Background and focus
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+                    title.setBackground(mTitleDefaultBackground);
+                }
+                else{
+                    title.setBackgroundDrawable(mTitleDefaultBackground);
+                }
+                title.setFocusable(true);
+                title.setFocusableInTouchMode(true);
+                title.requestFocus();
+                //Put the cursor at the end and open the keyboard
+                title.setSelection(title.getText().length());
+                InputMethodManager imm = (InputMethodManager)itemView.getContext()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+                //Switch the edit button for the save button
+                mBinding.editableListEntryEdit.setVisibility(View.GONE);
+                mBinding.editableListEntrySave.setVisibility(View.VISIBLE);
+            }
+            //Remove the action
+            else if (which == 1){
+                mListener.onDeleteItem(getAdapterPosition());
+                mDataset.remove(getAdapterPosition());
+                mAdapter.notifyItemRemoved(getAdapterPosition());
+            }
+        }
+
+        @Override
+        public void onClick(View view){
+            switch (view.getId()){
+                /*case R.id.custom_action_trigger:
+                    Log.d(TAG, "Editing the trigger of " + mCustomAction);
+
+                    //Open the trigger editor
+                    mListener.onEditTrigger(mCustomActions.get(getAdapterPosition()));
+                    break;*/
+
+                //When the user enters edition mode
+                case R.id.editable_list_entry_edit:
+                    AlertDialog dialog = new AlertDialog.Builder(itemView.getContext())
+                            .setTitle(R.string.editable_list_edit_dialog_title)
+                            .setItems(R.array.editable_list_edit_dialog_options, this)
+                            .create();
+                    dialog.show();
+                    break;
+
+                //When the user saves a currently existing goal (only from edition)
+                case R.id.custom_action_save:
+                    EditText title = mBinding.editableListEntryTitle;
+                    //Grab the title and check it ain't empty
+                    String newTitle = title.getText().toString().trim();
+                    if (newTitle.length() > 0){
+                        mListener.onEditItem(getAdapterPosition(), newTitle);
+
+                        //Hide the keyboard and make the title not focusable
+                        InputMethodManager imm2 = (InputMethodManager)itemView.getContext()
+                                .getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm2.hideSoftInputFromWindow(title.getWindowToken(), 0);
+                        title.clearFocus();
+                        title.setFocusable(false);
+                        title.setBackgroundResource(0);
+                        //Set the click listener again to start the trigger editor if the user taps
+                        title.setOnClickListener(this);
+
+                        //Swap the save button for the edit button
+                        mBinding.editableListEntryEdit.setVisibility(View.VISIBLE);
+                        mBinding.editableListEntrySave.setVisibility(View.GONE);
+                    }
+                    break;
+            }
         }
     }
 
